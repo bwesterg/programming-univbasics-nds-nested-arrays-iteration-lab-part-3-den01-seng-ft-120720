86e108d4d0aa3228dfcd12f6db79777b49dece9b
Benton Westergaard <benton.westergaard@gmail.com>
2020-11-23 20:03:03 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,8 +4,9 @@ def join_nested_strings(src)
   total = " "
 row_index = 0
 while row_index < src.count do
-  element_index = 0
+  element_index = ""
   while element_index < src[row_index].count do
+    if src[row_index][element_index][0] =/=
     total += src[row_index][element_index]
     element_index += 1
   end
