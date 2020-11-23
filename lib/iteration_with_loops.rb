921a280de0808d030e967300a420b40f5448cedd
Benton Westergaard <benton.westergaard@gmail.com>
2020-11-23 20:57:11 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,8 +6,7 @@ while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
     if src[row_index][element_index].is_a?(String) 
-      then a to total
-    total += src[row_index][element_index]
+      then total += src[row_index][element_index]
   
     element_index += 1
   end
