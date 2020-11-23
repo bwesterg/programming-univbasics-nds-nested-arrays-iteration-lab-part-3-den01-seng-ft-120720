b6d4f655b94a180c14d2aa2f0f8e29c3338e9c3d
Benton Westergaard <benton.westergaard@gmail.com>
2020-11-23 20:24:44 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,11 +5,12 @@ row_index = 0
 while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
-    if src[row_index][element_index] = String, do
+    
     total += src[row_index][element_index]
     element_index += 1
   end
   row_index += 1
 end
-join_nested_strings(src)
+total
+end
 end
